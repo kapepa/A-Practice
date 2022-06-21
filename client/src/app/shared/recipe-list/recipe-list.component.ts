e3d60dfc148763b0a7581b3d421738f86be261ca
kapepa <kapepayx@gmail.com>
2022-06-21 23:46:14 +0300

    recipe list

--- client/src/app/shared/recipe-list/recipe-list.component.ts
+++ client/src/app/shared/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DtoRecipe} from "../../dto/dto.recipe";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: DtoRecipe[] = [];
   constructor() { }
 
   ngOnInit(): void {
