2edc69beaa7c27024b293911e34e8bbd4ad85d74
kapepa <kapepayx@gmail.com>
2022-08-16 23:04:50 +0300

    test UrlPipe

--- client/src/app/pipe/url.pipe.spec.ts
+++ client/src/app/pipe/url.pipe.spec.ts
@@ -1,8 +1,9 @@
 import { UrlPipe } from './url.pipe';
 
 describe('UrlPipe', () => {
+  const pipe = new UrlPipe();
+
   it('create an instance', () => {
-    const pipe = new UrlPipe();
-    expect(pipe).toBeTruthy();
+    expect(pipe.transform('image')).toBe('http://127.0.0.1:5000/image');
   });
 });
