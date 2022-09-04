9f0d2b6b6e159b0e1813ce147368b39d692221be
kapepa <kapepayx@gmail.com>
2022-09-04 21:51:15 +0300

    unit test ErrorComponent

--- client/src/app/popup/error/error.component.spec.ts
+++ client/src/app/popup/error/error.component.spec.ts
@@ -1,15 +1,49 @@
-import { TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import { ErrorComponent } from './error.component';
+import {DtoErrorPopup} from "../../dto/dto.common";
+import {By} from "@angular/platform-browser";
 
 describe('ErrorComponent', () => {
+  let component: ErrorComponent;
+  let fixture: ComponentFixture<ErrorComponent>;
+
+  let error: DtoErrorPopup = {
+    open: true,
+    title: 'Title',
+    desc: 'Description',
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ErrorComponent ]
+      declarations: [
+        ErrorComponent
+      ]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(ErrorComponent);
+    component = fixture.componentInstance;
+
+    component.isError = error;
+    fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect([ ErrorComponent ]).toBeTruthy();
+    expect(component).toBeTruthy();
   });
+
+  it('should close error popup',() => {
+    spyOn(component, 'closePopUp').and.callThrough();
+    let popup = fixture.debugElement.query(By.css('#isError'));
+
+    component.close.pipe().subscribe((attr) => {
+      expect(attr).toBeUndefined();
+    })
+    popup.triggerEventHandler(
+      'click',
+      {stopPropagation: () => {}, currentTarget: {classList: { contains: (attr: string): boolean => attr === 'popup__zone' }} }
+    );
+
+    expect(component.closePopUp).toHaveBeenCalled();
+  })
 });
