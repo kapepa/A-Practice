6b895f0e54698f72fe51ed8163c6b3ad09d728ac
kapepa <kapepayx@gmail.com>
2023-03-29 18:00:13 +0300

    ci-cd

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -22,11 +22,11 @@ dotenv.config();
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.MYSQL_HOST,
-      port: Number(process.env.MYSQL_POST),
-      username: process.env.MYSQL_USERNAME,
-      password: process.env.MYSQL_ROOT_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
+      // host: process.env.MYSQL_HOST,
+      // port: Number(process.env.MYSQL_POST),
+      // username: process.env.MYSQL_USERNAME,
+      // password: process.env.MYSQL_ROOT_PASSWORD,
+      // database: process.env.MYSQL_DATABASE,
       entities: [
         User,
         Recipe,
